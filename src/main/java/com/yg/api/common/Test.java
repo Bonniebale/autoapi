a679f2d92f2fb8525faf61ea0420fa5a49043a58
guiwan <luguiwan@163.com>
2024-07-11 14:30:32 +0800

    1

---
+++ src/main/java/com/yg/api/common/Test.java
@@ -0,0 +1,9 @@
+package com.yg.api.common;
+
+/**
+ * @author flora
+ * @Title: Test
+ * @date 2024/7/11 11:19
+ */
+public class Test {
+}
---
+++ src/main/java/com/yg/api/common/utils/RequestUtil.java
@@ -0,0 +1,9 @@
+package com.yg.api.common.utils;
+
+/**
+ * @author: Flora
+ * @description:
+ * @date: 2024/7/11 11:29
+ */
+public class RequestUtil {
+}
